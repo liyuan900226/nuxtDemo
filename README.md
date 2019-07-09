4439e9cbd044861f16c14cd896d4e4e2de885883
liyuan <liyuan900226@163.com>
2019-07-09 21:52:49 +0800

    liyuan

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# nuxtDemo
+# mall
+
+> My super-duper Nuxt.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ npm run install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm run start
+
+# generate static project
+$ npm run generate
+```
+
+
+该项目中 sass vuex 路由都已配置好
+
+For detailed explanation on how things work, checkout [Nuxt.js docs](https://nuxtjs.org).
---
+++ pages/productList/_i.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <p>产品1</p>
+        <span>产品2</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "index",
+        mounted() {
+            this.$store.commit('mo1/todos/say')
+            this.$store.commit('say')
+        }
+    }
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+    @import "./assets/scss/common/_mixin";
+    div{
+        p{
+            color: $c-help;
+            //@include clearfix;
+            //@include ellipsis;
+        }
+        span{
+            color: red;
+        }
+    }
+</style>
